285eb3223d997997764c8c964af49dd1a205c0a7
Jonas Klock <jonas.klock@hotmail.de>
2022-06-14 15:03:12 +0200

    implement QtRoboEventType

--- QtRoboEvent.h
+++ QtRoboEvent.h
@@ -1,15 +1,17 @@
 #pragma once
 #include <iostream>
+#include "QtRoboEventType.h"
 
 class QtRoboEvent
 {
 private:
     u_int8_t mEventChannel;
     u_int8_t mEventValue;
+    QtRoboEventType mEventType;
 
 public:
     QtRoboEvent() = default;
-    QtRoboEvent(uint8_t channel, uint8_t value);
+    QtRoboEvent(uint8_t channel, uint8_t value, QtRoboEventType eventType);
     ~QtRoboEvent() = default;
 
     uint8_t eventChannel() const {
@@ -18,5 +20,9 @@ public:
 
     uint8_t eventValue() const {
         return mEventValue;
+    }
+
+    QtRoboEventType eventType() const {
+        return mEventType;
     }
 };
---
+++ QtRoboEventType.h
@@ -0,0 +1,1 @@
+enum class QtRoboEventType { binary, proportional };
