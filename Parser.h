a838de11e0b5c200469e0e3644c929407f8f4f17
Jonas Klock <jonas.klock@hotmail.de>
2022-06-14 15:16:55 +0200

    changed return type of parseToSumd to std::array

--- Parser.h
+++ Parser.h
@@ -14,7 +14,7 @@ public:
 
     QtRoboEvent parseToQtRoboEvent(char buffer[256]);
 
-    std::uint8_t* parseToSumd(const QtRoboEvent& event);
+    array<uint8_t, 9> parseToSumd(const QtRoboEvent& event);
 
     uint16_t crc16(uint16_t crc, uint8_t value);
 };
